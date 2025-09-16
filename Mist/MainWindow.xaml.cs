cae6ec6cbdfec25d540f4660bde32df1ab6b28fe
Brinley Hull <brinleyhull53@ku.edu>
2025-09-16 10:03:11 -0500

    Sound and people count simulated data

--- Mist/MainWindow.xaml.cs
+++ Mist/MainWindow.xaml.cs
@@ -18,20 +18,32 @@ namespace Mist;
 
 public partial class MainWindow : Window
 {
+    Random rnd;
+
     public MainWindow()
     {
         InitializeComponent();
+        rnd = new Random();
 
         DispatcherTimer timer = new DispatcherTimer();
-        timer.Tick += new EventHandler(UpdateTimer_Tick);
+        timer.Tick += new EventHandler(UpdateTimer_Second);
         timer.Interval = new TimeSpan(0, 0, 1); // updates every second
         timer.Start();
+
+        // Five second timer
+        DispatcherTimer timer_5sec = new DispatcherTimer();
+        timer_5sec.Tick += new EventHandler(UpdateTimer_FiveSecond);
+        timer_5sec.Interval = new TimeSpan(0, 0, 5); // hours, minutes, seconds
+        timer_5sec.Start();
     }
 
-    private void UpdateTimer_Tick(object sender, EventArgs e)
+    private void Timer_5sec_Tick(object? sender, EventArgs e)
     {
-        Random rnd = new Random();
+        throw new NotImplementedException();
+    }
 
+    private void UpdateTimer_Second(object sender, EventArgs e)
+    {
         // Calculate next heartrate value
         int currentHeartrate = Convert.ToInt16(HeartrateTextBlock.Text);
         int heartrateChange = rnd.Next(1, 6);
@@ -49,6 +61,29 @@ public partial class MainWindow : Window
         }
 
         HeartrateTextBlock.Text = currentHeartrate.ToString();
-        SoundTextBlock.Text = DateTime.Now.ToString();
+    }
+
+    private void UpdateTimer_FiveSecond(object sender, EventArgs e)
+    {
+        // Calculate next person value
+        int currentPeopleCount = Convert.ToInt16(PeopleTextBlock.Text);
+        int peopleCountChange = rnd.Next(0, 3);
+        if (currentPeopleCount < 3)
+        {
+            currentPeopleCount += peopleCountChange;
+        }
+        else
+        {
+            var signs = new[] { -1, 1 };
+            int sign = rnd.Next(2);
+            currentPeopleCount += signs[sign] * peopleCountChange;
+        }
+
+        // Calculate next sound level value
+        int currentSoundLevel = rnd.Next(30, 100);
+
+
+        PeopleTextBlock.Text = currentPeopleCount.ToString();
+        SoundTextBlock.Text = currentSoundLevel.ToString() + " dB";
     }
 }
