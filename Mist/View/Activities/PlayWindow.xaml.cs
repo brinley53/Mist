7ca8419ca612ce3873c9bc94a9807a43e845fd8d
Brinley Hull <brinleyhull53@ku.edu>
2025-11-10 21:11:10 -0600

    move around treat

--- Mist/View/Activities/PlayWindow.xaml.cs
+++ Mist/View/Activities/PlayWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Mist.View.Activities
@@ -20,6 +21,8 @@ namespace Mist.View.Activities
     /// </summary>
     public partial class PlayWindow : Window
     {
+        private bool draggingTreat;
+        private Point treatPos;
 
         private PlayViewModel vm;
 
@@ -28,6 +31,7 @@ namespace Mist.View.Activities
             InitializeComponent();
             vm = new PlayViewModel();
             DataContext = vm;
+            draggingTreat = false;
         }
 
         private void CloseButton_Click(object sender, RoutedEventArgs e)
@@ -53,6 +57,43 @@ namespace Mist.View.Activities
         private void ferret_MouseLeave(object sender, MouseEventArgs e)
         {
             vm.Hover = false;
+        }
+
+        private void Treat_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var treat = (UIElement)sender;
+            var parent = (Canvas)VisualTreeHelper.GetParent(treat);
+
+            // Get mouse position relative to the Canvas, not the Treat
+            var mousePos = e.GetPosition(parent);
+
+            // Calculate offset from Treat’s top-left corner
+            treatPos = new Point(
+                mousePos.X - Canvas.GetLeft(treat),
+                mousePos.Y - Canvas.GetTop(treat)
+            );
+
+            draggingTreat = true;
+            treat.CaptureMouse();
+        }
+
+        private void Treat_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            draggingTreat = false;
+            ((UIElement)sender).ReleaseMouseCapture();
+        }
+
+        private void Treat_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (draggingTreat)
+            {
+                var treat = (UIElement)sender;
+                var parent = (Canvas)VisualTreeHelper.GetParent(treat);
+                var mousePos = e.GetPosition(parent);
+
+                Canvas.SetLeft(treat, mousePos.X - treatPos.X);
+                Canvas.SetTop(treat, mousePos.Y - treatPos.Y);
+            }
         }
     }
 }
